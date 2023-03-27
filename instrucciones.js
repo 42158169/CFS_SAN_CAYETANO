41d2a22b107ba96f9484c54a6fbef88e943e7620
maira2023 <maira.roteno14@gmail.com>
2023-03-27 16:07:13 -0300

    panqueques

---
+++ instrucciones.js
@@ -0,0 +1,12 @@
+console.log("como hacer panqueques");
+console.log("ingredientes: harina 500g, 4 huevo, 1litro de leche");
+console.log("batir los huevo con la leche");
+console.log("agregar la harina a la mezcla");
+console.log("encender la ornalla");
+console.log("calentar la sarten");
+console.log("agregar a la sarten una cucharada de manteca");
+console.log("agregar la mezcla a la sarten");
+console.log("esperar que se cocine de un lado ");
+console.log("darlo vuelta");
+console.log("retirar de la sarten");
+console.log("repetir la secuencia hasta terminar la mezcla");
